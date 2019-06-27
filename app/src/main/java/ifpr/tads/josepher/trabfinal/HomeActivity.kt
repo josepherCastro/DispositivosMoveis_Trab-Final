f43d620e29c49dba91688b74d3c89ecb75ae98c4
Josepher José Castro da Silva <josephercastrosilva@gmail.com>
2019-06-27 12:59:39 +0000

    Autenticação google firebase

---
+++ app/src/main/java/ifpr/tads/josepher/trabfinal/HomeActivity.kt
@@ -0,0 +1,31 @@
+package ifpr.tads.josepher.trabfinal
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        fun signOut() {
+            startActivity(MainActivity.getLaunchIntent(this))
+            FirebaseAuth.getInstance().signOut()
+        }
+
+
+        sign_out_button.setOnClickListener {
+            signOut()
+        }
+    }
+    companion object {
+        fun getLaunchIntent(from: Context) = Intent(from, HomeActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+}
